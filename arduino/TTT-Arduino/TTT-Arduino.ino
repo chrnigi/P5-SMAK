67e3deb90e40e63fa12ca3b52d2a2643a5079d17
Morten <60173089+MortenSOlesen@users.noreply.github.com>
2025-11-03 13:32:57 +0100

    corrected win bitvector from 10 bits to 9

--- arduino/TTT-Arduino/TTT-Arduino.ino
+++ arduino/TTT-Arduino/TTT-Arduino.ino
@@ -61,7 +61,7 @@ int turn_counter = 0;
 // Bit vectors for checking if someone has won
 int victory_vectors [] = 
 {
-	0b1110000000, 0b000111000, 0b000000111, // rows
+	0b111000000, 0b000111000, 0b000000111, // rows
 	0b100100100, 0b010010010, 0b001001001, // columns
 	0b100010001, 0b001010100 // diagonals
 };
