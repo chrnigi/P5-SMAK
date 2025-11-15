8c7314e867c1ac208796fb3e457442d80632b210
theis999 <theis99999@gmail.com>
2025-11-15 20:06:09 +0100

    Added a bunch of unit test to test the correctness of move, capture, promotion and en passant.

--- ArduinoChess/ArduinoChess/test.cpp
+++ ArduinoChess/ArduinoChess/test.cpp
@@ -13,13 +13,53 @@ static int pin(std::string s) {
 
 	return (r | file);
 }
+
+TEST(pin, pin_to_string) {
+	int p = 18;
+	EXPECT_EQ(pin(pin_to_square(p)), p);
+	for (int i = 0; i < 63; i++)
+		EXPECT_EQ(pin(pin_to_square(i)), i);
+}
+
+TEST(state, reset) {
+	reset();
+	EXPECT_EQ(state, states::white);
+	EXPECT_EQ(chess_state.black_kingside, true);
+	EXPECT_EQ(chess_state.black_queenside, true);
+	EXPECT_EQ(chess_state.white_kingside, true);
+	EXPECT_EQ(chess_state.white_queenside, true);
+	EXPECT_EQ(chess_state.en_passant, false);
+	EXPECT_EQ(chess_state.en_passant_square, 0);
+	EXPECT_EQ(chess_state.ply, 0);
+	EXPECT_EQ(chess_state.ply_since_ponr, 0);
+	for (int i = 0; i < 64; i++) {
+		ASSERT_EQ(board[i], starting_board_initializer[i]);
+	}
+}
+TEST(state, clean_state) {
+	clean_state();
+	EXPECT_EQ(state, states::white);
+	EXPECT_EQ(chess_state.black_kingside, true);
+	EXPECT_EQ(chess_state.black_queenside, true);
+	EXPECT_EQ(chess_state.white_kingside, true);
+	EXPECT_EQ(chess_state.white_queenside, true);
+	EXPECT_EQ(chess_state.en_passant, false);
+	EXPECT_EQ(chess_state.en_passant_square, 0);
+	EXPECT_EQ(chess_state.ply, 0);
+	EXPECT_EQ(chess_state.ply_since_ponr, 0);
+	for (int i = 0; i < 64; i++) {
+		ASSERT_EQ(board[i], empty_board_initializer[i]);
+	}
+}
+
+TEST(FSM_white, WhiteTurn) {
 	clean_state();
 	int w = 31;
 	board[w] = p_WHITE_PAWN;
 	pin_change(w, true);
 	EXPECT_EQ(state, states::white_begin_move);
 }
-TEST(FSM, WhiteTurnEnemy) {
+TEST(FSM_white, WhiteTurnEnemy) {
 	clean_state();
 	int b = 22;
 	board[b] = p_BLACK_PAWN;
@@ -27,22 +67,41 @@ TEST(FSM, WhiteTurnEnemy) {
 	EXPECT_EQ(state, states::white_begin_enemy_capture);
 }
 
-
-TEST(FSM, UndoWhiteMove) {
+TEST(FSM_white, UndoWhiteMove) {
 	reset();
 	pin_change(63 - 8, true);
 	EXPECT_EQ(state, states::white_begin_move);
 	pin_change(63 - 8, false);
 	EXPECT_EQ(state, states::white);
 }
-TEST(FSM, FinishWhiteMove) {
+TEST(FSM_white, FinishWhiteMove) {
 	reset();
 	pin_change(63 - 8, true);
 	EXPECT_EQ(state, states::white_begin_move);
 	pin_change(63 - 16, false);
 	EXPECT_EQ(state, states::black);
 }
-TEST(FSM, FinishWhiteCapture) {
+
+TEST(FSM_white, StartWhiteCapture) {
+	reset();
+	int target = pin("a3");
+	int from = pin("b2");
+	board[target] = p_BLACK_PAWN;
+	pin_change(from, true);
+	pin_change(target, true);
+	EXPECT_EQ(state, states::white_begin_capture);
+}
+TEST(FSM_white, StartWhiteEnemyCapture) {
+	reset();
+	int target = pin("a3");
+	int from = pin("b2");
+	board[target] = p_BLACK_PAWN;
+	pin_change(target, true);
+	EXPECT_EQ(state, states::white_begin_enemy_capture);
+	pin_change(from, true);
+	EXPECT_EQ(state, states::white_begin_capture);
+}
+TEST(FSM_white, FinishWhiteCapture) {
 	reset();
 	board[63 - 17] = 'p';
 	pin_change(63 - 8, true);
@@ -52,7 +111,7 @@ TEST(FSM, FinishWhiteCapture) {
 	pin_change(63 - 17, false);
 	EXPECT_EQ(state, states::black);
 }
-TEST(FSM, FinishWhiteEnemyCapture) {
+TEST(FSM_white, FinishWhiteEnemyCapture) {
 	reset();
 	board[63 - 17] = 'p';
 	pin_change(63 - 17, true);
@@ -63,4 +122,332 @@ TEST(FSM, FinishWhiteEnemyCapture) {
 	EXPECT_EQ(state, states::black);
 }
 
+TEST(FSM_black, black_begin_move) {
+	clean_state();
+	state = states::black;
+	int from = pin("e5");
+	board[from] = p_BLACK_PAWN;
+	pin_change(from, true);
+	EXPECT_EQ(state, states::black_begin_move);
+}
+TEST(FSM_black, black_begin_enemy_capture) {
+	clean_state();
+	state = states::black;
+	int from = pin("d4");
+	board[from] = p_WHITE_PAWN;
+	pin_change(from, true);
+	EXPECT_EQ(state, states::black_begin_enemy_capture);
+}
 
+TEST(FSM_black, UndoBlackMove) {
+	clean_state();
+	state = states::black;
+	int from = pin("e5");
+	board[from] = p_BLACK_PAWN;
+	pin_change(from, up);
+	pin_change(from, down);
+	EXPECT_EQ(state, states::black);
+}
+TEST(FSM_black, FinalizeBlackMove) {
+	clean_state();
+	state = states::black;
+	int from = pin("e5");
+	int target = pin("e4");
+	board[from] = p_BLACK_PAWN;
+	pin_change(from, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white);
+}
+
+TEST(FSM_black, black_begin_capture) {
+	reset();
+	state = states::black;
+	int target = pin("a6");
+	int from = pin("b7");
+	board[target] = p_WHITE_PAWN;
+	pin_change(from, true);
+	pin_change(target, true);
+	EXPECT_EQ(state, states::black_begin_capture);
+}
+TEST(FSM_black, StartBlackEnemyCapture) {
+	reset();
+	state = states::black;
+	int target = pin("a6");
+	int from = pin("b7");
+	board[target] = p_WHITE_PAWN;
+	pin_change(target, true);
+	EXPECT_EQ(state, states::black_begin_enemy_capture);
+	pin_change(from, true);
+	EXPECT_EQ(state, states::black_begin_capture);
+}
+
+TEST(FSM_black, FinishBlackCapture) {
+	reset();
+	state = states::black;
+	int target = pin("a6");
+	int from = pin("b7");
+	board[target] = p_WHITE_PAWN;
+	pin_change(from, up);
+	EXPECT_EQ(state, states::black_begin_move);
+	pin_change(target, up);
+	EXPECT_EQ(state, states::black_begin_capture);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white);
+}
+TEST(FSM_black, FinishBlackEnemyCapture) {
+	reset();
+	state = states::black;
+	int target = pin("a6");
+	int from = pin("b7");
+	board[target] = p_WHITE_PAWN;
+	pin_change(target, up);
+	EXPECT_EQ(state, states::black_begin_enemy_capture);
+	pin_change(from, up);
+	EXPECT_EQ(state, states::black_begin_capture);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white);
+}
+
+TEST(FSM_promotion, WhitePromotion) {
+	clean_state();
+	int target = pin("a8");
+	int from = pin("a7");
+	board[from] = p_WHITE_PAWN;
+	pin_change(from, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::black);
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+	EXPECT_EQ(board[target], p_WHITE_QUEEN);
+
+	for (int i = 0; i < 8; i++) // run all files
+	{
+		clean_state();
+		int target = pin("a8") + i;
+		int from = pin("a7") + i;
+		board[from] = p_WHITE_PAWN;
+		pin_change(from, up);
+		pin_change(target, down);
+		EXPECT_EQ(state, states::black);
+		EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+		EXPECT_EQ(board[target], p_WHITE_QUEEN);
+	}
+}
+TEST(FSM_promotion, BlackPromotion) {
+	clean_state();
+	state = states::black;
+	int target = pin("a1");
+	int from = pin("a2");
+	board[from] = p_BLACK_PAWN;
+	pin_change(from, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white) << "Expected turn changed from black to white";
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE) << "Expected [from] square to be empty";
+	EXPECT_EQ(board[target], p_BLACK_QUEEN) << "Expected [target] square to have a queen";
+
+	for (int i = 0; i < 8; i++) // run all files
+	{
+		clean_state();
+		state = states::black;
+		int target = pin("a1") + i;
+		int from = pin("a2") + i;
+		board[from] = p_BLACK_PAWN;
+		pin_change(from, up);
+		pin_change(target, down);
+		EXPECT_EQ(state, states::white);
+		EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+		EXPECT_EQ(board[target], p_BLACK_QUEEN);
+	}
+}
+TEST(FSM_promotion, WhiteCapturePromotion) {
+	clean_state();
+	int target = pin("a8");
+	int from = pin("b7");
+	board[from] = p_WHITE_PAWN;
+	board[target] = p_BLACK_PAWN;
+	pin_change(from, up);
+	pin_change(target, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::black);
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+	EXPECT_EQ(board[target], p_WHITE_QUEEN);
+}
+TEST(FSM_promotion, WhiteEnemyCapturePromotion) {
+	clean_state();
+	int target = pin("a8");
+	int from = pin("b7");
+	board[from] = p_WHITE_PAWN;
+	board[target] = p_BLACK_PAWN;
+	pin_change(target, up);
+	EXPECT_EQ(state, states::white_begin_enemy_capture);
+	pin_change(from, up);
+	EXPECT_EQ(state, states::white_begin_capture);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::black);
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+	EXPECT_EQ(board[target], p_WHITE_QUEEN);
+}
+TEST(FSM_promotion, BlackCapturePromotion) {
+	clean_state();
+	state = states::black;
+	int target = pin("a1");
+	int from = pin("a2");
+	board[from] = p_BLACK_PAWN;
+	board[target] = p_WHITE_PAWN;
+	pin_change(from, up);
+	pin_change(target, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white) << "Expected turn changed from black to white";
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE) << "Expected [from] square to be empty";
+	EXPECT_EQ(board[target], p_BLACK_QUEEN) << "Expected [target] square to have a queen";
+}
+TEST(FSM_promotion, BlackEnemyCapturePromotion) {
+	clean_state();
+	state = states::black;
+	int target = pin("a1");
+	int from = pin("a2");
+	board[from] = p_BLACK_PAWN;
+	board[target] = p_WHITE_PAWN;
+	pin_change(target, up);
+	EXPECT_EQ(state, states::black_begin_enemy_capture);
+	pin_change(from, up);
+	EXPECT_EQ(state, states::black_begin_capture);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::white) << "Expected turn changed from black to white";
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE) << "Expected [from] square to be empty";
+	EXPECT_EQ(board[target], p_BLACK_QUEEN) << "Expected [target] square to have a queen";
+}
+TEST(FSM_promotion, White_NotPromotion) {
+	clean_state();
+	int target = pin("a8");
+	int from = pin("a7");
+	board[from] = p_WHITE_BISHOP;
+	pin_change(from, up);
+	pin_change(target, down);
+	EXPECT_EQ(state, states::black);
+	EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+	EXPECT_EQ(board[target], p_WHITE_BISHOP);
+
+	for (char piece : {p_WHITE_BISHOP, p_WHITE_KING, p_WHITE_KNIGHT, p_WHITE_QUEEN, p_WHITE_ROOK}) // run all pieces except pawn
+	{
+		clean_state();
+		chess_state.white_kingside = chess_state.white_queenside = false; // remove castling option
+		int target = pin("a8");
+		int from = pin("a7");
+		board[from] = piece;
+		pin_change(from, up);
+		pin_change(target, down);
+		EXPECT_EQ(state, states::black);
+		EXPECT_EQ(board[from], p_EMPTY_SQUARE);
+		EXPECT_EQ(board[target], piece);
+	}
+}
+
+TEST(FSM_en_passant, LongEnPassant) {
+	clean_state();
+	int from = pin("a2");
+	int to = pin("a4");
+	int en_passant_expected = pin("a3");
+	board[from] = p_WHITE_PAWN;
+	pin_change(from, up);
+	pin_change(to, down);
+	ASSERT_EQ(state, states::black);
+	EXPECT_TRUE(chess_state.en_passant);
+	ASSERT_EQ(chess_state.en_passant_square, en_passant_expected);
+
+	from = pin("h7");
+	to = pin("h5");
+	en_passant_expected = pin("h6");
+	board[from] = p_BLACK_PAWN;
+	pin_change(from, up);
+	pin_change(to, down);
+	EXPECT_TRUE(chess_state.en_passant);
+	ASSERT_EQ(chess_state.en_passant_square, en_passant_expected);
+
+	//not en passant
+	from = pin("a4");
+	to = pin("a5");
+	en_passant_expected = pin("a4");
+	pin_change(from, up);
+	pin_change(to, down);
+	EXPECT_FALSE(chess_state.en_passant);
+
+	from = pin("h5");
+	to = pin("h4");
+	en_passant_expected = pin("h5");
+	pin_change(from, up);
+	pin_change(to, down);
+	EXPECT_FALSE(chess_state.en_passant);
+
+	//set en passant
+	from = pin("g2");
+	to = pin("g4");
+	en_passant_expected = pin("g3");
+	board[from] = p_WHITE_PAWN;
+	pin_change(from, up);
+	pin_change(to, down);
+	ASSERT_EQ(state, states::black);
+	ASSERT_TRUE(chess_state.en_passant);
+	ASSERT_EQ(chess_state.en_passant_square, en_passant_expected);
+	//capture
+	from = pin("h4");
+	to = pin("g3");
+	int expected_empty = pin("g4");
+	pin_change(from, up);
+	pin_change(expected_empty, up);
+	pin_change(to, down);
+	ASSERT_EQ(state, states::white);
+	ASSERT_EQ(board[from], p_EMPTY_SQUARE);
+	ASSERT_EQ(board[to], p_BLACK_PAWN);
+	ASSERT_EQ(board[expected_empty], p_EMPTY_SQUARE);
+
+
+	//setup for en passant, move turn
+	state = states::black;
+	from = pin("b7");
+	to = pin("b5");
+	board[from] = p_BLACK_PAWN;
+	en_passant_expected = pin("b6");
+	pin_change(from, up);
+	pin_change(to, down);
+	ASSERT_TRUE(chess_state.en_passant);
+	ASSERT_EQ(chess_state.en_passant_square, en_passant_expected);
+	//capture
+	from = pin("a5");
+	to = pin("b6");
+	expected_empty = pin("b5");
+	pin_change(expected_empty, up);
+	pin_change(from, up);
+	pin_change(to, down);
+	ASSERT_EQ(state, states::black);
+	ASSERT_EQ(board[from], p_EMPTY_SQUARE);
+	ASSERT_EQ(board[to], p_WHITE_PAWN);
+	ASSERT_EQ(board[expected_empty], p_EMPTY_SQUARE);
+}
+TEST(FSM_en_passant, test_all_en_passant_squares) {
+	reset();
+	int to, from, en_passant;
+	ASSERT_EQ(state, states::white);
+	for (int i = 0; i < 8; i++) 
+	{
+		// WHITE
+		from = pin("a2") + i;
+		to = pin("a4") + i;
+		en_passant = pin("a3") + i;
+		pin_change(from, up);
+		pin_change(to, down);
+		ASSERT_EQ(state, states::black);
+		EXPECT_TRUE(chess_state.en_passant);
+		EXPECT_EQ(chess_state.en_passant_square, en_passant);
+
+		// BLACK
+		from = pin("a7") + i;
+		to = pin("a5") + i;
+		en_passant = pin("a6") + i;
+		pin_change(from, up);
+		pin_change(to, down);
+		ASSERT_EQ(state, states::white);
+		EXPECT_TRUE(chess_state.en_passant);
+		EXPECT_EQ(chess_state.en_passant_square, en_passant);
+	}
+	ASSERT_EQ(chess_state.ply, 16);
+}
