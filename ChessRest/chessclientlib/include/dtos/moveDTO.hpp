9aaf26973330a0aac997d66034e312c74009b9d6
chrnigi <chrinigin@gmail.com>
2025-11-19 21:58:24 +0100

    Add MoveType enum in preparation for changing to a simpler MoveDTO

--- ChessRest/chessclientlib/include/dtos/moveDTO.hpp
+++ ChessRest/chessclientlib/include/dtos/moveDTO.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "oatpp/core/base/Environment.hpp"
 #include <oatpp/core/Types.hpp>
 #include <oatpp/core/macro/codegen.hpp>
 #include <chesslib.hpp>
@@ -12,13 +13,36 @@ namespace smak { namespace models {
 #include OATPP_CODEGEN_BEGIN(DTO)
 
 ENUM(PiecesEnum, v_int32,
-    VALUE(KING, 0, "K"),
+    VALUE(KING, 0,  "K"),
     VALUE(QUEEN, 1, "Q"),
-    VALUE(ROOK, 2, "R"),
-    VALUE(BISHOP, 3, "B"),
-    VALUE(KNIGHT, 4, "N"),
-    VALUE(PAWN, 5, "P")
+    VALUE(ROOK, 2,  "R"),
+    VALUE(BISHOP, 3,"B"),
+    VALUE(KNIGHT, 4,"N"),
+    VALUE(PAWN, 5,  "P"),
+    VALUE(NONE, 6,  "Z")
 )
+
+// chess::Move values are 1 << 14 etc. so to get clean values in enum they are bitshifted back
+// The strings associated with these enum values MUST match the ones in MoveTypeEnum.java, else we run into JSON parsing issues. See below.
+ENUM(MoveType, v_int32,
+    VALUE(PROMOTION,    chess::Move::PROMOTION  >> 14, "promotion"),
+    VALUE(ENPASSANT,    chess::Move::ENPASSANT  >> 14, "enpassant"),
+    VALUE(CASTLING,     chess::Move::CASTLING   >> 14, "castling"),
+    VALUE(NORMAL,       chess::Move::NORMAL,           "normal"),
+    VALUE(NULLMOVE,     chess::Move::NULL_MOVE  >> 4,  "nullmove")
+)
+
+// Java version:
+/*********************************/
+// public enum MoveTypeEnum {
+//     PROMOTION("promotion"),
+//     ENPASSANT("enpassant"),
+//     CASTLES("castling"),
+//     NORMAL("normal"),
+//     NULLMOVE("nullmove");
+// }
+/*********************************/
+
 
 class MoveDTO : oatpp::DTO {
 
