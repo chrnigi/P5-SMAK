da94a84d39970acde8f94f273190e15291be62aa
chrnigi <chrinigin@gmail.com>
2025-11-22 18:59:36 +0100

    Actually function definitions now

--- ChessRest/uci_ipc/include/uci_commands.hpp
+++ ChessRest/uci_ipc/include/uci_commands.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #ifndef UCI_COMMANDS_HPP
 #define UCI_COMMANDS_HPP
 #include <chess.hpp>
@@ -7,10 +8,14 @@
 #include <initializer_list>
 
 /**
- * @brief Holds constants related to UCI commands.
- * 
+ * @brief UCI command factory class thingy.
+ * @note The commands are all wrapped in "\n <command>\n", due to how Stockfish interacts with its input pipe.
  */
 struct UCIcommand {
+
+private:
+    const std::string newlineify(std::string str);
+    UCIcommand() {}
 public:
 
     /**
@@ -33,87 +38,87 @@ public:
     /**
      * @brief Create the UCI command "position startpos"
      * 
-     * @return constexpr std::string_view 
+     * @return constexpr "\n position startpos\n" 
      */
     constexpr static std::string_view position_startpos() { return "\n position startpos\n"; };
     
     /**
-     * @fn std::string_view create_position_command(std::string_view fen)
+     * @fn std::string create_position_command(std::string_view fen)
      * @brief Create a UCI command in the form of "position <fen>".
      * 
      * @param fen The FEN string to set as position.
-     * @return std::string_view 
+     * @return std::string
      */
-    static std::string_view create_position_command(std::string_view fen);
+    static std::string create_position_command(std::string_view fen);
     
     /**
-     * @fn std::string_view create_position_command(std::string_view fen, std::initializer_list<chess::Move> moves)
+     * @fn std::string create_position_command(std::string_view fen, std::vector<chess::Move> moves)
      * @brief Create a UCI command in the form of "position <fen> <moves>".
      * 
      * @param fen The FEN string to start the moves from.
      * @param moves Moves to play from the position given in the FEN string.
-     * @return std::string_view 
+     * @return std::string 
      */
-    static std::string_view create_position_command(std::string_view fen, std::initializer_list<chess::Move> moves);
+    static std::string create_position_command(std::string_view fen, std::vector<chess::Move> moves);
+
+    // /**
+    //  * @fn std::string create_position_command(std::vector<chess::Move> moves)
+    //  * @brief Create a UCI command in the form of "position <moves>"
+    //  * @param moves Moves to play from the engine's currently stored position.
+    //  * @return std::string
+    //  */
+    // static std::string create_position_command(std::vector<chess::Move> moves);
 
     /**
-     * @fn std::string_view create_position_command(std::initializer_list<chess::Move> moves)
-     * @brief Create a UCI command in the form of "position <moves>"
-     * @param moves Moves to play from the engine's currently stored position.
-     * @return std::string_view 
-     */
-    static std::string_view create_position_command(std::initializer_list<chess::Move> moves);
-
-    /**
-     * @fn std::string_view append_moves_to_position_command(std::string pos_command, chess::Move move)
-     * @brief Convenience overload for <tt>append_moves_to_position_command(std::string pos_command, std::initializer_list<chess::Move> moves)</tt>
+     * @fn std::string append_moves_to_position_command(std::string_view pos_command, chess::Move move)
+     * @brief Convenience overload for <tt>append_moves_to_position_command(std::string pos_command, std::vector<chess::Move> moves)</tt>
      * 
      * @param pos_command The UCI position command.
      * @param move The move to append to the given position command.
-     * @return std::string_view 
+     * @return std::string
      */
-    static std::string_view append_moves_to_position_command(std::string pos_command, chess::Move move);
+    static std::string append_moves_to_position_command(std::string_view pos_command, chess::Move move);
 
     /**
-     * @fn std::string_view append_moves_to_position_command(std::string pos_command, std::initializer_list<chess::Move> moves)
+     * @fn std::string append_moves_to_position_command(std::string_view pos_command, std::vector<chess::Move> moves)
      * @brief Appends a list of moves to an already existing UCI position command.
      * 
      * @param pos_command The UCI position command.
-     * @param moves Initializer list of moves to append to the command.
-     * @return std::string_view 
+     * @param moves @p std::vector of moves to append to the command.
+     * @return std::string 
      */
-    static std::string_view append_moves_to_position_command(std::string pos_command, std::initializer_list<chess::Move> moves);
+    static std::string append_moves_to_position_command(std::string_view pos_command, std::vector<chess::Move> moves);
 
     /**
      * @brief Create a UCI "go depth" command with a given depth of @p depth
      * 
      * @param depth The desired search depth.
-     * @return std::string_view 
+     * @return std::string 
      */
-    static std::string_view create_go_depth_command(unsigned int depth);
+    static std::string create_go_depth_command(unsigned int depth = 20);
 
     /**
      * @brief Get the UCI "stop" command.
      * 
-     * @return constexpr std::string_view 
+     * @return constexpr "\n stop\n"
      */
     static constexpr std::string_view stop()        { return "\n stop\n"; }
     /**
      * @brief Get the UCI "quit" command.
      * 
-     * @return constexpr std::string_view 
+     * @return constexpr "\n quit\n"
      */
     static constexpr std::string_view quit()        { return "\n quit\n"; }
     /**
      * @brief Get the UCI "ucinewgame" command.
      * 
-     * @return constexpr std::string_view 
+     * @return constexpr "\n ucinewgame\n"
      */
     static constexpr std::string_view ucinewgame()  { return "\n ucinewgame\n"; }
     /**
      * @brief Get the UCI "ponderhit" command.
      * 
-     * @return constexpr std::string_view 
+     * @return constexpr "\n ponderhit\n"
      */
     static constexpr std::string_view ponderhit()   { return "\n ponderhit\n"; }
 
---
+++ ChessRest/uci_ipc/src/uci_commands.cpp
@@ -0,0 +1,48 @@
+#include "chess.hpp"
+#include <string>
+#include <string_view>
+#include <uci_commands.hpp>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/compile.h>
+#include <vector>
+
+
+
+const std::string UCIcommand::newlineify(std::string str) {
+    return fmt::format(FMT_COMPILE("\n {}\n"), str);
+} 
+
+std::string UCIcommand::create_position_command(std::string_view fen) {
+    return fmt::format(FMT_COMPILE("\n position {}\n"), fen);
+}
+
+std::string UCIcommand::create_position_command(std::string_view fen, std::vector<chess::Move> moves) {    
+    std::vector<std::string> mvs;
+    mvs.reserve(moves.size());
+
+    for (auto m : moves) {
+        mvs.push_back(chess::uci::moveToUci(m));
+    }
+    return fmt::format(FMT_COMPILE("\n position {} moves {}\n"), fen, fmt::join(mvs, " "));
+}
+
+std::string UCIcommand::append_moves_to_position_command(std::string_view pos_command, chess::Move move) {
+    return append_moves_to_position_command(pos_command, {move});
+}
+
+std::string UCIcommand::append_moves_to_position_command(std::string_view pos_command, std::vector<chess::Move> moves) {
+    std::vector<std::string> mvs;
+    mvs.reserve(moves.size());
+
+
+    for (auto m : moves) {
+        mvs.push_back(chess::uci::moveToUci(m));
+    }
+
+    return fmt::format(FMT_COMPILE("\n {} {}\n"), pos_command, fmt::join(mvs, " "));
+}
+
+std::string UCIcommand::create_go_depth_command(unsigned int depth) {
+    return fmt::format(FMT_COMPILE("\n go depth {}\n"), depth);
+}
